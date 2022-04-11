683992290cacc12f40f0f090583212729945c307
Nitish Kumar kushwaha <67130565+Nitish-Kumar-kushwaha@users.noreply.github.com>
2022-04-11 15:00:12 +0530

    2nd-commit

--- termwork1.py
+++ termwork1.py
@@ -1,3 +1,92 @@
+#basic
+stack =[]
+
+stack.append('a')
+stack.append('b')
+stack.append('c')
+
+print(f"Initial stack {stack}")
+
+
+print(stack.pop())
+print(stack.pop())
+print(stack.pop())
+print(f"stack after element popped: {stack}")
+
+
+queue=[]
+queue.append('a')
+queue.append('b')
+queue.append('c')
+queue.append('d')
+
+print(f"Initial queue {queue}")
+
+print(queue.pop())
+print(queue.pop())
+print(queue.pop())
+
+print(f"queue after element popped: {queue}")
+
+#DFS
+
+from collections import defaultdict
+
+class Graph:
+
+    def __init__(self):
+        self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def BFS(self, s):
+        visited = [False] * (max(self.graph) + 1)
+
+        queue = []
+
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            s = queue.pop(0)
+            print(s, end = " ")
+
+            for i in self.graph[s]:
+                if visited[i] == False:
+                    queue.append(i)
+                    visited[i] = True
+
+    def DFSUtil(self, v, visited):
+        visited.add(v)
+        print(v, end=" ")
+
+        for neighbour in self.graph[v]:
+            if neighbour not in visited:
+                self.DFSUtil(neighbour, visited)
+
+    def DFS(self, v):
+        visited = set()
+
+        self.DFSUtil(v, visited)
+
+g = Graph()
+g.addEdge(0, 1)
+g.addEdge(0, 2)
+g.addEdge(1, 2)
+g.addEdge(2, 0)
+g.addEdge(2, 3)
+g.addEdge(3, 3)
+
+print("Following is Breadth First Traversal (starting from vertex 2)")
+g.BFS(2)
+print("\n")
+print("Following is Depth First Traversal (starting from vertex 2)")
+g.DFS(2)
+
+
+# main program
+
 graph = {
     'A' : ['B', 'C'],
     'B' : ['D'],
